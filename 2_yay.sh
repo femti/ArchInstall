26501f8ea42ef54f14803abdd46fcea168e516a7
femti <vlad@qunter.com>
2023-08-24 21:06:42 +0200

    Update 2_yay.sh

--- 2_yay.sh
+++ 2_yay.sh
@@ -14,7 +14,7 @@ YAY
 )
 
 echo '==== Inatall YAY packages ===='
-su -l wlad <<< $(cat <<< AURPROG
+su -l wlad <<< $(cat << AURPROG
 yay -S --noconfirm --needed timeshift pycharm-community-edition raindrop evernote tradingview  insync-nautilus insync ttf-jetbrains-mono-nerd ttf-noto-nerd plymouth-theme-monoarch gnome-shell-extension-rounded-window-corners gnome-shell-extension-dash-to-dock gnome-shell-extension-openweather gnome-shell-extension-appindicator gnome-shell-extension-blur-my-shell gnome-shell-extension-gsconnect gnome-shell-extension-gtk4-desktop-icons-ng gnome-shell-extension-status-area-horizontal-spacing-git
 AURPROG
 )
